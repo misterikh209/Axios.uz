f29706686ec2ff3334d35ae96c2bcf124b5f09b1
misterikh209 <saitovixtiyor209@gmail.com>
2024-05-19 17:49:54 +0500

    connect

---
+++ script.js
@@ -0,0 +1,337 @@
+document.addEventListener("DOMContentLoaded", async function(e){
+     let d = document
+     let container = d.querySelector(".container")
+     
+     let req = await fetch("https://axios.uz/index.php/api/getMovies",{
+          method: "GET"
+     })
+     let resp = await req.json()
+     
+     resp.forEach(function(e){
+          let card = d.createElement("div")
+          card.classList.add("card", "bg-dark")
+          card.setAttribute("style", "width:18rem;")
+          card.innerHTML = `
+          <img src="./images/movie.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title" style="color: #fff;">${e.title}</h5>
+            <ul class="list-group list-group-flush">
+               <li id="li1" class="list-group-item bg-dark" style="padding-top:1px solid #fff";><span style="color:white;">${e.release}</span></li>
+               <li class="list-group-item bg-dark" style="color: white;">ID: ${e.id}</li>
+               <li id="li2" class="list-group-item bg-dark" style="padding-bottom:1px solid #fff"><span style="color:white;">${e.duration}</span></li>
+            </ul>
+             <span style="display:flex;gap:7px;">
+             <a href="#" class="btn btn-primary">Смотреть</a>
+             <a href="#" class="btn btn-danger" id="del">Удалить</a>
+             </span>
+          </div>
+          `
+          container.append(card)
+     })
+
+     ///////////////////////////////////////////////////////////////////////
+     /// Главное меню click
+
+     let mainMenuBtn = document.getElementById("mainMenu")
+     mainMenuBtn.addEventListener("click", async function(e){
+          container.innerHTML = ``
+          resp.forEach(function(e){
+               let card = d.createElement("div")
+               card.classList.add("card", "bg-dark")
+               card.setAttribute("style", "width:18rem;")
+               card.innerHTML = `
+               <img src="./images/movie.png" class="card-img-top" alt="...">
+               <div class="card-body">
+                 <h5 class="card-title" style="color: #fff;">${e.title}</h5>
+                 <ul class="list-group list-group-flush">
+                    <li id="li1" class="list-group-item bg-dark" style="padding-top:1px solid #fff";><span style="color:white;">${e.release}</span></li>
+                    <li class="list-group-item bg-dark" style="color: white;">ID: ${e.id}</li>
+                    <li id="li2" class="list-group-item bg-dark" style="padding-bottom:1px solid #fff"><span style="color:white;">${e.duration}</span></li>
+                 </ul>
+                  <span style="display:flex;gap:7px;">
+                  <a href="#" class="btn btn-primary">Смотреть</a>
+                  <a href="#" class="btn btn-danger" id="del">Удалить</a>
+                  </span>
+               </div>
+               `
+               container.append(card)
+
+               d.body.style.backgroundImage = "none"
+               document.querySelector(".navbar").classList.add("bg-dark")
+               document.getElementById("signUp").classList.remove("btn-primary")
+               document.getElementById("signUp").classList.add("btn-warning")
+               d.body.style.backgroundColor = "#131313"
+               d.querySelector(".search-box").style.display = "flex"
+               ///////animation
+               let animation = document.querySelector(".weather-nav")
+               setTimeout(()=>{
+                    animation.style.top = "-30px"
+               }, 1000)
+          })
+     })
+
+     ///////////////////////////////////////////////////////////////////////
+     /// Войти click
+
+     d.getElementById("login").addEventListener("click", async function(e){
+          container.innerHTML = `
+          <div class="content">
+          <h1 style="text-align: center;">Login</h1>
+          <div class="inputs">
+          
+               <input type="text" placeholder="Username">
+               <input type="password" placeholder="Parol">
+          </div>
+          <div class="remFor">
+               <div class="remember">
+                    <input type="checkbox" id="checkBox">
+                    <label for="checkBox">Meni eslab qol</label>
+               </div>
+               <a href="#">Forgot password</a>
+          </div>
+          <button class="loginBtn">Login</button>
+          <p style="text-align: center;padding-top: 15px;">Sizda akkaunt yo'qmi? <span><a href="#">Ro'yxatdan o'tish</a></span></p>
+          </div>
+          `
+          d.body.style.backgroundImage = "url(./images/login-bg.jpg)"
+          d.body.style.backgroundRepeat = "no-repeat"
+          d.body.style.backgroundSize = "cover"
+          d.querySelector(".navbar").classList.remove("bg-dark")
+          d.getElementById("signUp").classList.remove("btn-warning")
+          d.getElementById("signUp").classList.add("btn-primary")
+          d.querySelector(".search-box").style.display = "none"
+          d.querySelector(".stick").style.background = "rgba(255, 255, 255, .1)"
+
+          ///////animation
+          let animation = document.querySelector(".weather-nav")
+          setTimeout(()=>{
+               animation.style.top = "-30px"
+          }, 1000)
+     })
+
+     ///////////////////////////////////////////////////////////////////////
+     /// зарегистрироваться click
+     
+     d.getElementById("signUp").addEventListener("click", async function(e){
+          container.innerHTML = `
+          <div class="content">
+               <h1 style="text-align: center;">Sign Up</h1>
+               <div class="inputs">
+               
+                    <input type="text" placeholder="Your Name">
+                    <select name="" id="">
+                         <option value="">Frontend-20</option>
+                         <option value="">Frontend-21</option>
+                         <option value="">Frontend-22</option>
+                         <option value="">Frontend-23</option>
+                         <option value="">Frontend-24</option>
+                         <option value="">Frontend-25</option>
+                    </select>
+                    <input type="text" placeholder="Username">
+                    <input type="password" placeholder="Password">
+               </div>
+               <button class="loginBtn">Ro'yxatdan o'tish</button>
+               
+          </div>
+          `
+          d.getElementById("searchInp").style.background = "transparent"
+          d.getElementById("searchInp").style.backdropFilter = "blur(9px)"
+          d.getElementById("searchInp").style.border = "2px solid rgba(255, 255, 255, .2)"
+          d.body.style.backgroundImage = "url(./images/sign-in-bg.jpg)"
+          d.body.style.backgroundRepeat = "no-repeat"
+          d.body.style.backgroundSize = "cover"
+          d.querySelector(".navbar").classList.remove("bg-dark")
+          d.getElementById("signUp").classList.remove("btn-warning")
+          d.getElementById("signUp").classList.add("btn-primary")
+          d.querySelector(".search-box").style.display = "none"
+          d.querySelector(".stick").style.background = "rgba(255, 255, 255, .1)"     
+          
+          ///////animation
+          let animation = document.querySelector(".weather-nav")
+          setTimeout(()=>{
+               animation.style.top = "-30px"
+          }, 1000)
+     })
+     ///////////////////////////////////////////////////////////////////////
+     /// Актёры click
+
+     d.getElementById("actor").addEventListener("click", async function(e){
+          container.innerHTML = ``
+          let request = await fetch("https://axios.uz/index.php/api/getActors")
+          let responsive = await request.json()
+
+          responsive.forEach(e=>{
+               let card = d.createElement("div")
+               card.classList.add("col3")
+               card.innerHTML = ` 
+               <div class="card bg-dark" style="width: 18rem;">
+               <img src="${e.photo}" class="card-img-top" alt="...">
+               <div class="card-body">
+                 <h5 class="card-title" style="color:#fff;">${e.name}</h5>
+                 <p class="card-text" style="color:#fff;">${e.movies.map(t=>t.title)}</p>
+                 <a href="#" class="btn btn-primary">Смотреть</a>
+                 <a href="#" class="btn btn-danger">Удалить</a>
+               </div>
+               </div>
+               `
+               container.append(card)
+               d.body.style.backgroundImage = "none"
+               document.querySelector(".navbar").classList.add("bg-dark")
+               document.getElementById("signUp").classList.remove("btn-primary")
+               document.getElementById("signUp").classList.add("btn-warning")
+               // d.body.style.backgroundImage = "none"
+               d.body.style.backgroundColor = "#131313"
+               d.querySelector(".search-box").style.display = "flex"
+
+               ///////animation
+               let animation = document.querySelector(".weather-nav")
+               setTimeout(()=>{
+                    animation.style.top = "-30px"
+               }, 1000)
+          })
+     })
+     d.getElementById("searchBtn").addEventListener("click", async function(e){
+          container.innerHTML = ``
+          let searchInp = d.getElementById("searchInp").value
+          let request = await fetch(`https://axios.uz/index.php/api/searchMovie?search=${searchInp}`, {
+               method:"GET"
+          })
+          let response = await request.json()
+          let mov = response.data
+
+          mov.forEach(m=>{
+               let card = d.createElement("div")
+               card.classList.add("col3")
+               card.innerHTML = ` 
+               <div class="card bg-dark" style="width: 18rem;">
+               <img style="height:360px;" src="./images/movie.png" class="card-img-top" alt="...">
+               <div class="card-body">
+                 <h5 class="card-title" style="color:#fff;">${m.title}</h5>
+                 <p class="card-text" style="color:#fff;">${m.duration}</p>
+                 <a href="#" class="btn btn-primary">Смотреть</a>
+                 <a href="#" class="btn btn-danger">Удалить</a>
+               </div>
+               </div>
+               `
+
+               container.append(card)
+               
+          })
+
+          let request_2 = await fetch(`https://axios.uz/index.php/api/searchActor?search=${searchInp}`, {
+               method:"GET"
+          })
+          let response_2 = await request_2.json()
+          let actor_data = response_2.data
+          actor_data.forEach(ad=>{
+               let card = d.createElement("div")
+               card.classList.add("col3")
+               card.innerHTML = ` 
+               <div class="card bg-dark" style="width: 18rem;">
+               <img src="${ad.photo}" class="card-img-top" alt="...">
+               <div class="card-body">
+                 <h5 class="card-title" style="color:#fff;">${ad.name}</h5>
+                 <p class="card-text" style="color:#fff;">${ad.movies.map(r=>r.release)}</p>
+                 <a href="#" class="btn btn-primary">Смотреть</a>
+                 <a href="#" class="btn btn-danger">Удалить</a>
+               </div>
+               </div>
+               `
+               container.append(card)
+          })
+          d.getElementById("searchInp").value = ""
+     })
+
+     ///////////////////////////////////////////////////////////////////////
+     /// Погода click
+
+     document.getElementById("weatherPage").addEventListener("click", async function(e){
+          container.innerHTML = ``
+          
+          d.querySelector(".navbar").classList.remove("bg-dark")
+          d.body.style.background = `url(./weather-bg.jpg)`
+          d.body.style.backgroundSize = `cover`
+          d.body.classList.add("body-weather")    
+          d.getElementById("signUp").classList.remove("btn-warning")
+          d.getElementById("signUp").classList.add("btn-primary")
+          d.querySelector(".search-box").style.display = "none"
+          d.querySelector(".stick").style.background = "rgba(255, 255, 255, .1)"
+          container.innerHTML = `
+          <div class="content2">
+          <div class="search-box2">
+               <input id="inp2" type="text" placeholder="Search weather">
+               <button id="btn2">
+                    <ion-icon name="search-outline"></ion-icon>
+               </button>
+          </div>
+          <h1 id="text2" style="text-align: center;">Weather Search</h1>
+          <div class="img-box2">
+               <img src="../weather.png" alt="" id="img2">
+          </div>
+          <h1 id="weather2" style="text-align: center;"></h1>
+          </div>
+          `
+
+          
+
+
+          document.getElementById("btn2").addEventListener("click", async function(e){
+               let inp = document.getElementById("inp2").value.trim()
+               let request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inp}&units=metric&appid=827e02ac6f41dc6dbe450c07fa86c420`)
+               let response = await request.json()
+               let weatherText = document.getElementById("weather2")
+               // console.log(response.name);
+               // console.log(response.main.temp);
+               // console.log(response.weather[0].main);
+                
+               let img = document.getElementById("img2")
+               console.log(response.weather[0].main);
+               document.getElementById("text2").innerHTML = `${response.name} ${response.main.temp}°C`
+               if(response.weather[0].main == "Sunny"){
+                    img.setAttribute("src", `https://cdn1.iconfinder.com/data/icons/weather-color-3/515/weather-06-1024.png`)
+                    weatherText.innerHTML = `Quyoshli`
+               } else if(response.weather[0].main == "Clouds"){
+                    img.setAttribute("src","./weather/cloud.png")
+                    weatherText.innerHTML = `Bulutli`
+               } else if(response.weather[0].main == "Clear"){
+                    img.setAttribute("src","./weather/clear-sky.png")
+                    weatherText.innerHTML = `Musaffo osmon`
+               } else if(response.weather[0].main == "Rain"){
+                    img.setAttribute("src","./weather/rain.png")
+                    weatherText.innerHTML = `Yomg'irli`
+               } 
+               let animation = document.getElementById("weatherNav")
+               setTimeout(()=>{
+                    animation.style.top = "-150px"
+               }, 1000)
+               document.getElementById("inp2").value = ""
+          })
+          
+          
+          
+     })
+
+     //////////////////////////////////////////////////////////////////////////////////////////
+     //Weather Navbar
+     let requestWeatherNav = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Navoiy&units=metric&appid=827e02ac6f41dc6dbe450c07fa86c420`)
+     let responseWeatherNav = await requestWeatherNav.json()
+     
+     let weatherNavImg = document.getElementById("weatherNavImg")
+     d.getElementById("weatherNavName").innerHTML = responseWeatherNav.name
+     d.getElementById("weatherNavGr").innerHTML = `${responseWeatherNav.main.temp}°C`
+     if(responseWeatherNav.weather[0].main == "Sunny"){
+          document.getElementById("weatherNavText").innerHTML = `Quyoshli`
+          weatherNavImg.setAttribute("src", "https://cdn1.iconfinder.com/data/icons/weather-color-3/515/weather-06-1024.png")
+     } else if(responseWeatherNav.weather[0].main == "Clouds"){
+          document.getElementById("weatherNavText").innerHTML = `Bulutli`
+          weatherNavImg.setAttribute("src", "./weather/cloud.png")
+     } else if(responseWeatherNav.weather[0].main == "Clear"){
+          document.getElementById("weatherNavText").innerHTML = `Ochiq`
+          weatherNavImg.setAttribute("src", "./weather/clear-sky.png")
+     } else if(responseWeatherNav.weather[0].main == "Rain"){
+          document.getElementById("weatherNavText").innerHTML = `Yomg'irli`
+          weatherNavImg.setAttribute("src", "./weather/rain.png")
+     }
+     
+     
+})
